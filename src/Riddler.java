7af066b376e04e92c8adf80d44f4ab538b5b843e
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2024-09-02 12:52:40 -0700

    code

--- src/Riddler.java
+++ src/Riddler.java
@@ -9,8 +9,28 @@ import java.util.ArrayList;
  * Completed by: AGASTYA BRAHMBHATT
  */
 public class Riddler {
-    // Riddle One is a cesar shift
+    /*
+    Riddle 1 is a cesar shift, so I converted everything into ascii and depending on whether
+    it was punctuation, a capital letter, and/or if it was before or after R (so it could loop around),
+    I either shifted it up or down towards the correct ascii number it should be, and then converted it back into a char.
+     */
     public String decryptOne(String encrypted) {
+        //New way of doing it
+        String decrypted = "";
+        Character shifted;
+        for(int i = 0; i < encrypted.length();i++){
+            if((encrypted.charAt(i) >= 'A') || (encrypted.charAt(i) <= 'Z'))
+            {
+                shifted = (char)((encrypted.charAt(i) - 'A' + 9) % 26 + 'A');
+                decrypted += shifted.toString();
+            }
+            else if((encrypted.charAt(i) >= 'a') || (encrypted.charAt(i) <= 'z'))
+            {
+                shifted = (char)((encrypted.charAt(i) - 'a' + 9) % 26 + 'a');
+                decrypted += shifted.toString();
+            }
+        }
+        /* old way of doing it
         String decrypted = "";
         char shiftedBy = 'j' - 'a';
         char loopBy = 'R' - 'A';
@@ -31,11 +51,16 @@ public class Riddler {
             }
             decrypted += decryptedChar.toString();
         }
+         */
         System.out.println(decrypted);
         return decrypted;
     }
 
-    // Riddle two uses ascii conversion to each character
+    /*
+    Riddle two uses ascii conversion to each character, so I simply used parseInt to convert each ascii
+    number into a char and returned it in decrypted form. I used a regex of space because each number is separated by a space
+    and used a.Trim() in order for parseInt() to work properly.
+     */
     public String decryptTwo(String encrypted) {
         String decrypted = "";
         Character decryptedChar = ' ';
@@ -51,10 +76,20 @@ public class Riddler {
         return decrypted;
     }
 
-    // Riddle three used 8 binary digits to represent each character
+    /*
+       Riddle three used 8 binary digits to represent each character,
+       so decryptThree uses a regex of separating into 8 binary characters
+       in order to convert each binary character using parseInt()
+     */
     public String decryptThree(String encrypted) {
         String decrypted = "";
         Character decryptedChar = ' ';
+        /*
+        \\G does a global match and not just a match in the beginning of the string.
+        The regular expression below is splitting the string matching any characters
+        into substrings of 8 characters.
+         */
+
         String[] numArray = encrypted.split("(?<=\\G.{8})");
         String aTrim;
         for(String a : numArray)
@@ -67,7 +102,14 @@ public class Riddler {
         return decrypted;
     }
 
-    // Riddle four uses a unicode shift of 9919, which using ascii, can give us each individual character.
+    /*
+    Riddle four uses a unicode shift of 9919,
+    which using ascii, can give us each individual character.
+    The decryptFour() method initially converts the character into an int.
+    Using trial and error, the method eventually worked with substracting
+    the integer form of the character by the number 9919 to get its correct ascii value.
+    Then, I converted the ascii values for each back into chars.
+     */
     public String decryptFour(String encrypted) {
         String decrypted = "";
         Character decryptedChar = ' ';
